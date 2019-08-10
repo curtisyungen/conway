56db1ff95aef3a42c5735fe1efbb1eb6fd57ae76
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-10 16:01:20 -0700

    Add files via upload

---
+++ src/components/Board/board.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Cell from "../Cell/cell";
+import "./board.css";
+
+class Board extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="board">
+
+            </div>
+        )
+    }
+}
+
+export default Board;
