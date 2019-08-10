9087410ecc84a42a2fda99a861cfe304f3ad5968
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-10 15:59:34 -0700

    Add files via upload

---
+++ src/components/Cell/cell.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import ".cell/css";
+
+class Cell extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    render() { 
+        return (
+            <div className="cell">
+
+            </div>
+        )
+    }
+}
+
+export default Cell;
