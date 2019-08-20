0c9a31c3dfed719fcd135840c1dfc34ca2faddc9
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-20 09:06:14 -0700

    Add files via upload

--- src/utils/patterns.js
+++ src/utils/patterns.js
@@ -41,13 +41,13 @@ module.exports = {
             pattern.push(row);
         }
 
-        pattern[3][2].val = true;
-        pattern[3][3].val = true;
-        pattern[4][2].val = true;
-        pattern[4][3].val = true;
-        pattern[5][3].val = true;
-        pattern[6][3].val = true;
-        pattern[7][3].val = true;
+        pattern[9][11].val = true;
+        pattern[9][12].val = true;
+        pattern[10][11].val = true;
+        pattern[10][12].val = true;
+        pattern[11][12].val = true;
+        pattern[12][12].val = true;
+        pattern[13][12].val = true;
 
         return pattern;
     }
