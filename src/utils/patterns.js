82434562254579db9b06179842d108080d47225a
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-20 10:51:30 -0700

    Add files via upload

--- src/utils/patterns.js
+++ src/utils/patterns.js
@@ -23,23 +23,7 @@ module.exports = {
     },
 
     getPattern2() {
-        let pattern = [];
-
-        for (var i=0; i<25; i++) {
-            let row = [];
-
-            for (var j=0; j<25; j++) {
-                let cell = {
-                    row: i,
-                    col: j,
-                    val: false,
-                }
-
-                row.push(cell);
-            }
-
-            pattern.push(row);
-        }
+        let pattern = this.getPattern1();
 
         pattern[9][11].val = true;
         pattern[9][12].val = true;
@@ -50,6 +34,15 @@ module.exports = {
         pattern[13][12].val = true;
 
         return pattern;
-    }
+    },
 
+    getPattern3() {
+        let pattern = this.getPattern1();
+
+        for (var i=7; i<17; i++) {
+            pattern[i][12].val = true;
+        }
+        
+        return pattern;
+    }
 }
