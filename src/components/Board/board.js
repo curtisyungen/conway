77407fa8814c37e62b42a4b23a44ee1c2f7381cb
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-20 12:12:41 -0700

    Add files via upload

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -112,7 +112,7 @@ class Board extends Component {
                 let cell = {
                     row: i,
                     col: j,
-                    val: (Math.random() * 50) < 35,
+                    val: (Math.random() * 50) < 15,
                 }
 
                 row.push(cell);
